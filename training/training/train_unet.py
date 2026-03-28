7da9220ed7dccbfa53dca98bcdf81575d178edfd
Chandan Kumar Vanjarapu <130856627+chandan112307@users.noreply.github.com>
2026-03-28 07:40:09 +0530

    Comment out model.summary() in train_unet.py
    Comment out model summary to reduce output during training.

--- training/training/train_unet.py
+++ training/training/train_unet.py
@@ -289,7 +289,7 @@ def train(
     # Model
     # ------------------------------------------------------------------
     model = build_unet_model(learning_rate=learning_rate)
-    model.summary()
+    # model.summary()
 
     # ------------------------------------------------------------------
     # Callbacks
